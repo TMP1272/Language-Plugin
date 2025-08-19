928a86421e85fd09ea7dfe04266c9432d2be0e0d
TMP1272 <161656592+TMP1272@users.noreply.github.com>
2025-08-19 15:37:54 +0700

    Create Main.java

---
+++ app/src/vi/java/com/tmp1272/languege_vi/Main.java
@@ -0,0 +1,11 @@
+package com.tmp1272.languege_vi;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
